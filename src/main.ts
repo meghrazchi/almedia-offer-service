18692c72060701f638414cf9c8d042017ac39fa5
Behzad Meghrazchi <behzad.meghrazchi@gmail.com>
2024-04-29 19:35:06 +0000

    add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  // app.useGlobalPipes(new ValidationPipe()) // comment this because the validation is not applied on the routes
   const config = new DocumentBuilder()
     .setTitle('Offer API Documentation')
     .setDescription('Providing API to manage offers')
